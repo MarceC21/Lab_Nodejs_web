3c080ec7c01e5f1794765d764d2955125e9a27b2
MarceC21 <marcelap.castillo2102@gmail.com>
2026-04-21 18:45:36 -0600

    feat: configuración módulos y cambios en ruta info

---
+++ Parte2/server-bueno.js
@@ -0,0 +1,55 @@
+
+import http from "http"
+import fs from "fs/promises"
+import path from "path"
+
+
+const PORT = 3001
+
+// Crear el servidor HTTP
+const server = http.createServer(async (req, res) => {
+
+  // Ruta raíz  
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end("Servidor activo")
+    return
+  }
+
+  // Ruta de información
+  if (req.url === "/info") {
+    res.writeHead(200, { "Content-Type": "application/json" })
+    const info = {
+        Mensaje: "La info del servidor/lab",
+        Curso: "Sistemas y tecologías web",
+        Tecnología: "JavaScript y Node.js"
+    }
+    res.end(JSON.stringify(info))
+    return
+}
+
+
+  // Ruta de estudiantes
+  //Tiene Try Catch para manejar errores al leer el archivo
+  
+  if (req.url === "/api/student") {
+    try {
+      const filePath = path.join(process.cwd(), "datos.json")
+      const texto = await fs.readFile(filePath, "utf-8") //Se puso await para esperar a que se lea el archivo antes de continuar
+      res.writeHead(200, { "Content-Type": "application/json" })
+      res.end(texto) // //Se cambio a texto para enviar el contenido del archivo como respuesta
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" })
+      res.end("Error, archivo no encontrado")
+    }
+    return
+  }
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Ruta no encontrada")
+}); // Se debe cerrar el bloque de código del createServer
+
+
+server.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost:3001")
+}); // Se debe cerrar el bloque de código del listen
+
